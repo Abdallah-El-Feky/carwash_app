8d4c7df918a7aa4faa8d0618a0f5fd520eb1e6f2
Alyy2021 <semor2013@gmail.com>
2022-08-12 17:20:04 +0200

    Aly12-

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,8 +1,6 @@
-//
 import UIKit
 import Flutter
-import GoogleMaps
-// import gmap
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
@@ -10,25 +8,6 @@ import GoogleMaps
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-    GMSServices.provideAPIKey("AIzaSyAoBjMXbI8k18vKn3y-wd0MuYawx4eN3GQ")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-
   }
 }
-
-// import UIKit
-// import Flutter
-// import GoogleMaps // Add this line!
-//
-// @UIApplicationMain
-// @objc class AppDelegate: FlutterAppDelegate {
-//   override func application(
-//     _ application: UIApplication,
-//     didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?
-//   ) -> Bool {
-//     GeneratedPluginRegistrant.register(with: self.body)
-//     GMSServices.provideAPIKey("AIzaSyAoBjMXbI8k18vKn3y-wd0MuYawx4eN3GQ")  // Add this line!
-//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-//   }
-// }
-
